9dccee3a548ef479711c7de8b04943ba5f5fe825
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-12-30 09:36:14 +0100

    https://to-backendapi-v1-x0zt.onrender.com

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.use((req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
     res.setHeader(
         'Access-Control-Allow-Origin',
-        'http://localhost:5173'
-        //  'https://www.to-analytics.com'
+        'http://localhost:5173',
+         'https://www.to-analytics.com'
     );
     // res.setHeader('Access-Content-Allow-Orgin', 'https://www.to-analytics.com')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH,DELTE')
