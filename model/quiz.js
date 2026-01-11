07347543bac8845c90b050a2a3cedce35293d5fa
mayowasamuel <fpasamuelmayowa51@gmail.com>
2026-01-11 08:47:46 +0100

    nene

--- model/quiz.js
+++ model/quiz.js
@@ -6,7 +6,11 @@ const questionSchema = new Schema({
   question: { type: String, required: true },
   options: [{ type: String, required: true }],
   correct: { type: [String], required: true }, // supports multiple correct answers
-  reason: { type: String } 
+  reason: { type: String } ,
+   multi: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 // ✅ Quiz schema (wraps questions together)
